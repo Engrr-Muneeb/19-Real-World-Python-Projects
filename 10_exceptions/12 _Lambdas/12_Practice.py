c7e21b920f5c40d45c1bb2ef0989629e870b7620
asma-033 <asmamaqbool35@gmail.com>
2024-01-10 17:56:45 +0500

    Add 12_Lambdas_Map_Filter

--- 10_exceptions/12 _Lambdas/12_Practice.py
+++ 10_exceptions/12 _Lambdas/12_Practice.py
@@ -1,22 +1,22 @@
 #lambda
-#result = (lambda x=10,y=20: x+y)(y=50)
-#print(result)
+result = (lambda x=10,y=20: x+y)(y=50)
+print(result)
 
 #map
-#number = [1,2,3,4,5]
+number = [1,2,3,4,5]
 
-#def square(x):
-#    return x*x
+def square(x):
+    return x*x
 
-#new_list = list(map(square,number))
-#print(new_list)
+new_list = list(map(square,number))
+print(new_list)
 
 
 #filter
-#numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,2,3,4,5,6,7,8,9,10]
 
-#odd_numbers = list(filter(lambda x: x%2==1,numbers))
-#print(odd_numbers)
+odd_numbers = list(filter(lambda x: x%2==1,numbers))
+print(odd_numbers)
 
 #generators
 def even_generator(x):
@@ -24,5 +24,3 @@ def even_generator(x):
         if i%2==0:
             yield i
 print(list(even_generator(11)))
-
-
